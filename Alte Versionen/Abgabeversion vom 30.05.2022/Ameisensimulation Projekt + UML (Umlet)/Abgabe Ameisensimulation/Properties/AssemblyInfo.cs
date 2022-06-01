38a6c448280775abd83ddfecedb985432ae77c74
Karl Klotz <karl.klotz@steep.de>
2022-06-01 12:17:16 +0200

    Projektabschluss Archivierung inklusive Analyse und Bewertungsgedanken

---
+++ Alte Versionen/Abgabeversion vom 30.05.2022/Ameisensimulation Projekt + UML (Umlet)/Abgabe Ameisensimulation/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Abgabe Ameisensimulation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("steep GmbH ")]
+[assembly: AssemblyProduct("Abgabe Ameisensimulation")]
+[assembly: AssemblyCopyright("Copyright © steep GmbH  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("03550fe1-9fba-4b3b-8edc-b1b981b01a36")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
